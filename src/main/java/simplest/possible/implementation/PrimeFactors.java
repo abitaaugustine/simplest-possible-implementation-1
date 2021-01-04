cb6805a3217162b02c5080589558bc25966bc54f
Abita Ann Augustine <abitaaugustine@gmail.com>
2021-01-04 15:18:34 +0530

    Create PrimeFactors.java

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+
+    public <Integer> List<Integer> of(int number) {
+        List<Integer> prime_factors=new ArrayList<Integer>();
+        return prime_factors;
+    }
+}
